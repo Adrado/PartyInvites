07cc1af3816b2417b5f7d73d37ea509eabb949d6
Fidel <fidel.mh3@gmail.com>
2021-02-03 13:14:51 +0100

    Init PartyInvites project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
